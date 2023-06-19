4745bf36f1e329e2e76143fea74daa965d5110ed
Ico Davids <ico.davids@gmail.com>
2023-06-19 08:52:37 +0200

    Meta tags for robots and description

--- site/index.php
+++ site/index.php
@@ -4,7 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-   	<title>Nederlands voor Oekraïners</title>
+   	<meta name="robots" content="noindex,nofollow"/>
+    <meta name="description" content="Materiaal en links bij de cursus Nederlands voor Oekraïners."/>
+    <title>Nederlands voor Oekraïners</title>
     <link rel="stylesheet" href="style.css">
   </head>
   <body>
